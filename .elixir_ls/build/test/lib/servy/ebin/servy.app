e71f037038f6a25c5a44e99f6bd30e880cb14534
john wesonga <johnwesonga@gmail.com>
2020-04-25 14:44:23 -0700

    organized code into different modules

--- .elixir_ls/build/test/lib/servy/ebin/servy.app
+++ .elixir_ls/build/test/lib/servy/ebin/servy.app
@@ -1,6 +1,7 @@
 {application,servy,
              [{applications,[kernel,stdlib,elixir,logger]},
               {description,"servy"},
-              {modules,['Elixir.Servy','Elixir.Servy.Handler']},
+              {modules,['Elixir.Servy','Elixir.Servy.Handler',
+                        'Elixir.Servy.Parser','Elixir.Servy.Plugins']},
               {registered,[]},
               {vsn,"0.1.0"}]}.
--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,4 +1,15 @@
 defmodule Servy.Handler do
+  @moduledoc """
+  Handles HTTP Requests
+  """
+
+  @pages_path Path.expand("../../pages", __DIR__)
+  import Servy.Plugins, only: [rewrite_path: 1, log: 1, track: 1]
+  import Servy.Parser
+
+  @doc """
+  Transforms the request into a response
+  """
   def handle(request) do
     request
     |> parse
@@ -8,35 +19,6 @@ defmodule Servy.Handler do
     |> track
     |> format_response
   end
-
-  def track(%{status: 404, path: path} = conv) do
-    IO.puts("Warning: #{path} is on the loose!")
-    conv
-  end
-
-  def track(conv), do: conv
-
-  def rewrite_path(%{path: "/wildlife"} = conv) do
-    %{conv | path: "/wildthings"}
-  end
-
-  def rewrite_path(conv), do: conv
-
-  def log(conv), do: IO.inspect(conv)
-
-  def parse(request) do
-    [method, path, _] =
-      request
-      |> String.split("\n")
-      |> List.first()
-      |> String.split(" ")
-
-    %{method: method, path: path, resp_body: "", status: nil}
-  end
-
-  # def route(conv) do
-  #  route(conv, conv.method, conv.path)
-  # end
 
   def route(%{method: "GET", path: "/wildthings"} = conv) do
     %{conv | status: 200, resp_body: "Bears, Tigers, Lions"}
@@ -51,13 +33,20 @@ defmodule Servy.Handler do
   end
 
   def route(%{method: "GET", path: "/about"} = conv) do
-    file =
-      Path.expand("../../pages", __DIR__)
+    @pages_path
       |> Path.join("about.html")
       |> File.read()
       |> handle_file(conv)
   end
 
+  def route(%{path: path} = conv) do
+    %{conv | status: 404, resp_body: "No #{path} found here!"}
+  end
+
+  @spec handle_file({:error, any} | {:ok, any}, %{resp_body: any, status: any}) :: %{
+          resp_body: any,
+          status: 200 | 404 | 500
+        }
   def handle_file({:ok, content}, conv) do
     %{conv | status: 200, resp_body: content}
   end
@@ -87,9 +76,7 @@ defmodule Servy.Handler do
   #   # end
   # end
 
-  def route(%{path: path} = conv) do
-    %{conv | status: 404, resp_body: "No #{path} found here!"}
-  end
+
 
   def format_response(conv) do
     """
