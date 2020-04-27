762834b65ebbc8e1b6af5bd76f6ac364e3a5d489
john wesonga <johnwesonga@gmail.com>
2020-04-26 21:30:34 -0700

    processed headers using recursion

--- lib/servy/conv.ex
+++ lib/servy/conv.ex
@@ -1,17 +1,16 @@
 defmodule Servy.Conv do
-defstruct method: "", path: "", status: nil, resp_body: "", params: %{}
+  defstruct method: "", path: "", status: nil, resp_body: "", params: %{}, headers: %{}
 
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      404 => "Not Found",
+      500 => "Internal Server Error",
+      201 => "Created"
+    }[code]
+  end
 
-defp status_reason(code) do
-  %{
-    200 => "OK",
-    404 => "Not Found",
-    500 => "Internal Server Error",
-    201 => "Created"
-  }[code]
-end
-
-def full_status(conv) do
-  "#{conv.status} #{status_reason(conv.status)}"
-end
+  def full_status(conv) do
+    "#{conv.status} #{status_reason(conv.status)}"
+  end
 end
